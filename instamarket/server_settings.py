30b84fc2a6f32526d4578f38dc0e891dd5d390fa
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-08-13 18:42:25 +0430

    ...

--- instamarket/server_settings.py
+++ instamarket/server_settings.py
@@ -30,4 +30,4 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS=['/home/khafonli/intamarket/static']
+STATICFILES_DIRS=['/home/khafonli/instamarket/static']
