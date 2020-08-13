bbdacc3ed78bdd849cae28aa8e6525a6525922ca
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-08-13 23:56:03 +0430

    ...

--- instamarket/settings.py
+++ instamarket/settings.py
@@ -45,8 +45,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if ON_HEROKU: MIDDLEWARE.append(
-    'whitenoise.middleware.WhiteNoiseMiddleware')
+if ON_HEROKU: 
+    MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+    
 ROOT_URLCONF = 'instamarket.urls'
 
 TEMPLATES = [
