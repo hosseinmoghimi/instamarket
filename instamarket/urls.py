789063854827904cbc708517ea2cd811ee4eb302
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-08-14 16:22:10 +0430

    ...

--- instamarket/urls.py
+++ instamarket/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('',include('dashboard.urls')),
 
 
-    url(r'^'+MEDIA_URL[1:]+'(?P<path>.*)$', serve,{'document_root': MEDIA_ROOT}), 
-    url(r'^'+STATIC_URL[1:]+'(?P<path>.*)$', serve,{'document_root': STATIC_ROOT}),  
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': STATIC_ROOT}),  
 ]
 
 
