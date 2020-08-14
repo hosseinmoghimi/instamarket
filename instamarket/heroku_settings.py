5f33770e7059f982076463776e49b1c4f408ff91
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-08-14 18:13:36 +0430

    ....

--- instamarket/heroku_settings.py
+++ instamarket/heroku_settings.py
@@ -6,10 +6,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-secret_file_path=os.path.join(os.path.join(BASE_DIR, 'secret'),'secret.sec')
-
-with open(secret_file_path) as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = '5a@8$h^m0-jtc+w7%xnd7q8r2pct8v77+p^+jt-%&-x8&iw92r'
 
 
 ALLOWED_HOSTS=['instamarket-django.herokuapp.com','www.herokuapp.com','dashboard.heroku.com','www.heroku.com','heroku.com']
